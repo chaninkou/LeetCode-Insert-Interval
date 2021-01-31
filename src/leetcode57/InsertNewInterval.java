e879676ea391d3e1724a294f91d6f8492f8c0dc2
chaninkou <michaelkoulove@yahoo.com>
2021-01-30 20:39:34 -0800

    Redid problem for better understanding.

--- src/leetcode57/InsertNewInterval.java
+++ src/leetcode57/InsertNewInterval.java
@@ -17,8 +17,8 @@ public class InsertNewInterval {
 
 		// Update the new Interval accordingly to min and max
 		while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-			newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-			newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+			newInterval[0] = Math.min(newInterval[0], intervals[i][0]); // Start
+			newInterval[1] = Math.max(newInterval[1], intervals[i][1]); // End
 			i++;
 		}
 
